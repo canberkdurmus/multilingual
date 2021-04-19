3453c781a697fec507610784012e452edd6bc1ac
Can Berk Durmus <canberkdurmus@gmail.com>
2021-04-20 01:08:24 +0300

    Add driver code

---
+++ main.py
@@ -0,0 +1,7 @@
+from multilingual import Multilingual
+
+if __name__ == '__main__':
+    ml = Multilingual(target_language='fr')
+    print(ml.main_list)
+    print(ml.words)
+    print(ml.get('hello'))
